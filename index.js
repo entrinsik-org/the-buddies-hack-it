d083d9e4b5ef9198ed7d1e747ccf37121c31c3b5
Robin Lamb <robin@bedrock.local>
2018-12-19 13:49:30 -0500

    load flowsteps from function on server start

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 'use strict';
 exports.register = function(server,opts,next){
-    server.drivers('flow',require('./lib'));
+    //need to server on start handler
+    //get all the functions
+    //findall with namespace informer-flow
+    //attach each one to the server flow drivers
+    //server.drivers('flow',require('./lib'));
+    server.on ('start',async () => {
+       const FunctionModel = server.app.db.model('Function');
+       const flowFunctions = await FunctionModel.findAll({where:{namespace:'informer-flow'}});
+       const flowDrivers = flowFunctions.map(f=>eval(`(${f.script})`));
+       server.drivers('flow', flowDrivers);
+    });
     next();
 };
 exports.register.attributes={name:'SumOfArray'};
