6891019b20f0f4d3ba4437ef78b636529c3c6f6b
amoro99 <andrew@entrinsik.com>
2019-01-16 10:27:26 -0500

    Refine form and function

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 'use strict';
-const driverfactory = require('./lib/driverfactory');
+const DriverFactory = require('./lib/driver-factory').DriverFactory;
 
-exports.register = function(server,opts,next){
-    //need to server on start handler
-    //get all the functions
-    //findall with namespace informer-flow
-    //attach each one to the server flow drivers
-    //server.drivers('flow',require('./lib'));
-    server.on ('start',async () => {
-       const FunctionModel = server.app.db.model('Function');
-       const flowFunctions = await FunctionModel.findAll({where:{namespace:'informer-flow'}});
-       const flowDrivers = flowFunctions.map(f=>driverfactory(f));
-       server.drivers('flow', flowDrivers);
-       //add hook to function save
-       FunctionModel.hook('afterSave',function(funct,opts){
-        server.drivers('flow', driverfactory(funct));
-       })
+exports.register = function (server, opts, next) {
+    const driverFactory = new DriverFactory();
+    server.on('start', async () => {
+        const FunctionModel = server.app.db.model('Function');
+        const flowFunctions = await FunctionModel.findAll({ where: { namespace: 'informer-flow' } });
+        const flowDrivers = flowFunctions.map(f => driverFactory.createDriver(f));
+        server.drivers('flow', flowDrivers);
+
+        FunctionModel.hook('afterCreate', funct => {
+            if (funct.namespace === 'informer-flow') {
+                const driver = driverFactory.createDriver(funct);
+                server.driver('flow', driver);
+            }
+        });
+
+        FunctionModel.hook('afterUpdate', funct => {
+            const flowDriverManager = server.driverManager('flow');
+            if (funct.previous('namespace') === 'informer-flow') 
+                flowDriverManager.remove(funct.previous('name'));
+            if (funct.namespace === 'informer-flow') 
+                flowDriverManager.add(driverFactory.createDriver(funct));
+        });
+
+        FunctionModel.hook('afterDestroy', funct => {
+            if (funct.namespace === 'informer-flow') {
+                const flowDriverManager = server.driverManager('flow');
+                flowDriverManager.remove(funct.name);
+            }
+        });
+
     });
     next();
 };
-exports.register.attributes={name:'SumOfArray'};
+
+exports.register.attributes = { name: 'CustomFlows' };
